64fbd74365019c7900df4960dee65d0f070ace4c
Fleny <fleny113@outlook.com>
2025-04-18 20:37:47 +0200

    format and use addWeek in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "includes/game.hpp" // Non sarebbe necessario (già incluso in savemanager.hpp) ma lo mettiamo per chiarezza
 #include "includes/savemanager.hpp"
+#include "includes/simulation.hpp"
 #include "includes/utils.hpp"
 #include <cstdlib>
 #include <iostream>
@@ -10,7 +11,6 @@ void menu(City citta);
 
 int main() {
     City citta;
-    int tempo=0;
     srand((unsigned int)time(NULL));
 
     // NOTA: Questo utilizza la cartella di output di cmake per il file di salvataggio
@@ -19,35 +19,42 @@ int main() {
         citta = createNewCity();
     }
 
-    tempo = calcolaTempo(citta);
     cout << citta.name << " - SETTIMANA " << citta.time.week << " - MESE " << citta.time.month << " - ANNO " << citta.time.year << endl;
     menu(citta);
 
     return 0;
 }
 
-void menu(City citta){
-    int scelta=0, salva=0;
-    do{
+void menu(City citta) {
+    int scelta = 0, salva = 0;
+    do {
         cout << "1) Stampa informazioni citta'" << endl;
         cout << "2) Stampa condizioni servizi" << endl;
         cout << "3) Vai avanti di una settimana" << endl;
         cout << "4) Salva i progressi e continua" << endl;
         cout << "0) Esci dal gioco senza salvare" << endl;
-        do{
+
+        do {
             cout << "Scegli un'azione: ";
             cin >> scelta;
-            if(scelta<0 || scelta>5) cout << "Inserimento errato. Riprova.";
-        }while(scelta<0 || scelta>5);
-        switch(scelta){
-            case 1: stampaInfo(citta); break;
-            case 2: break; // TODO
-            case 3: citta.time.week++; break;
+            if (scelta < 0 || scelta > 5) cout << "Inserimento errato. Riprova.";
+        } while (scelta < 0 || scelta > 5);
+
+        switch (scelta) {
+            case 1:
+                stampaInfo(citta);
+                break;
+            case 2:
+                break; // TODO
+            case 3:
+                addWeek(citta.time);
+                break;
             case 4:
-                if(saveCity("citta.txt", citta)) cout << "Salvataggio effettuato." << endl;
+                if (saveCity("citta.txt", citta)) cout << "Salvataggio effettuato." << endl;
                 else cerr << "ERRORE: Salvataggio fallito. Controlla che il file sia disponibile e riprova." << endl;
                 break;
-            default: break;
+            default:
+                break;
         }
-    }while(scelta!=0);
+    } while (scelta != 0);
 }
